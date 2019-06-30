4d319928021f29b6ed1b344fced18c472ea5bffc
mingbaba <33719626+mingbaba@users.noreply.github.com>
2019-06-30 19:11:32 +0800

    xcz
    Signed-off-by: mingbaba <33719626+mingbaba@users.noreply.github.com>

--- app/src/main/java/com/example/lucky_project/MainActivity.java
+++ app/src/main/java/com/example/lucky_project/MainActivity.java
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
+import cn.bmob.v3.Bmob;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String a="";
-        int e=0;
-        int f=1;
-        int e1e=123;
-        int  df=12312;
+        Bmob.initialize(this, "ae52cfb3930ff0a825aab5cf155f5f8d");
+
+
+
     }
+
 }
